6f7a83c337078c716019bbd209edc21d7ae9096b
Doris-Lam <doriswylam@gmail.com>
2026-05-23 00:08:38 -0400

    clean up

--- components/conditional-doodles.tsx
+++
@@ -1,3 +0,0 @@
-export function ConditionalDoodles() {
-  return null
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,12 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        'brother-signature': ['BrotherSignature', 'cursive'],
         serif: ['var(--font-serif)', 'Georgia', 'serif'],
         sans: ['var(--font-mono)', 'ui-monospace', 'monospace'],
         mono: ['var(--font-mono)', 'ui-monospace', 'monospace'],
@@ -17,4 +15,4 @@ module.exports = {
     },
   },
   plugins: [],
-}; 
+};
